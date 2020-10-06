3db7a8b3bbc8b207cc136c94def60d90259219eb
Felippe Marques <ambrsp@hotmail.com>
2020-10-05 23:21:20 -0300

    VERSÃO 1.5

--- venda.c
+++ venda.c
@@ -10,12 +10,14 @@
 #include <stdlib.h>
 #include <locale.h>
 #include <ctype.h>
+#include <conio.h>
 
 #define N 5
 typedef struct{
 	int cod_produto;
 	char nome_produto[40];
 	float custo_produto;
+	float custo_total;
 }cadastro;
 
 cadastro lista[N] = {
@@ -42,18 +44,22 @@ void exibicao(){
 }
 
 int calculo_produto(){
+	FILE *ponteiro_arquivo; // variável tipo arquivo.	
+
 	int i=0,indice=0,unit=0;
 	char opcao,letra;	
-	float total=0;
+	float total=0,soma_total=0;
+	
 	
 	printf("\n");
-		
 	do{
+		ponteiro_arquivo = fopen("registro/registro_pagamento.txt","a"); // IRÁ CRIAR O ARQUIVO DENTRO DA PASTA REGISTRO (W) = escrita
 		indice = 0;
-		printf("SELECIONE O CÓDIGO DO PRODUTO:");
+		printf("SELECIONE O CÓDIGO DO PRODUTO : ");
 		scanf("%i",&indice);	
+		
 		if((indice >= 0) && (indice<=4)){
-			printf("QUANTAS UNIDADES?");
+			printf("DIGITE O NÚMERO DE UNIDADES : ");
 			scanf("%i",&unit);
 				
 			if(unit == 0){
@@ -64,8 +70,21 @@ int calculo_produto(){
 			system("cls");
 			printf("**********NOTA FISCAL**********\n");
 			printf("\nRAZÃO SOCIAL : MICKEY E DONALD");
-			printf("\nNOME DO PRODUTO : %s\nPREÇO UNITÁRIO : R$ %.2f\nQUANTIDADE ADQUIRIDA : %i UNIDADES \n\nVALOR TOTAL : R$ %.2f\n\n",
+			printf("\nNOME DO PRODUTO : %s\nPREÇO UNITÁRIO : R$ %.2f\nQUANTIDADE ADQUIRIDA : %i UNIDADES \n\nSUBTOTAL TOTAL : R$ %.2f\n\n",
 			lista[indice].nome_produto,lista[indice].custo_produto,unit,total);
+			
+			//ESCRITA NO ARQUIVO CRIADO A PARTIR DA VARIÁVEL PONTEIRO_ARQUIVO
+			
+			
+			fprintf(ponteiro_arquivo,"RAZÃO SOCIAL : MICKEY E DONALD\nCÓDIGO DO PRODUTO : %i\n",lista[indice].cod_produto);
+			fprintf(ponteiro_arquivo,"NOME DO PRODUTO : %s\nPREÇO UNITÁRIO : R$ %.2f\n",lista[indice].nome_produto,lista[indice].custo_produto);
+			fprintf(ponteiro_arquivo,"QUANTIDADE ADQUIRIDA : %i UNIDADES\nVALOR TOTAL : R$ %.2f\n\n",unit,total);
+			fprintf(ponteiro_arquivo,"\n");
+
+			fclose(ponteiro_arquivo);
+			
+			////////////////////////////////////////////////////////////////////
+			
 			system("pause");
 			system("cls");
 			topo();	
@@ -78,20 +97,53 @@ int calculo_produto(){
 			topo();	
 			exibicao();
 		}
-		printf("\nDESEJA CONTINUAR ? [S/N]");
+		soma_total += total;
+		
+		if(soma_total!=0){
+			printf("TOTAL : %.2f\n",soma_total);
+			printf("----------------------------------------------------------\n");
+		}
+		printf("\nDESEJA CONTINUAR [S/N]?");
 		fflush(stdin);
 		scanf("%s",&opcao);	
+		//printf("\n");
 		letra = toupper(opcao);
 		
 	}while(letra !='N');
 }
 
+int pagamento_produto(){
+	int pagamento=0,parcela=0;
+	do{
+		system("cls");
+		printf("\n>>>MODO DE PAGAMENTO<<<\n\n");
+		printf("1 ) DINHEIRO\n2 ) CARTÃO DE CRÉDITO\n3 ) CHEQUE\n\n");	
+		printf("SELECIONA UMA OPÇÃO VÁLIDA : ");
+		scanf("%i",&pagamento);
+
+		switch(pagamento){
+			case 1 : 
+				printf("\nFORMA DE PAGAMENTO : DINHEIRO\n");
+				 printf("QUANTAS PARCELAS : ");		 
+				 scanf("%i",&parcela);
+			break;
+			case 2 : printf("\nFORMA DE PAGAMENTO : CARTÃO DE CRÉDITO");
+			
+			break;
+			case 3 : printf("\nFORMA DE PAGAMENTO : CHEQUE");
+	
+			break;
+		}
+	}while(pagamento>3 || pagamento==0);
+}
+
+
 int main(){
 	setlocale(LC_ALL,"PORTUGUESE");		
-	
 	topo();
 	exibicao();
 	calculo_produto();
+	pagamento_produto();
 	
 }
 
